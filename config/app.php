354b4fa707cf792ac7ec8af899316f621b1a42f1
Ridho Idris <ridhoidris@gmail.com>
2019-08-08 21:58:02 +0700

    fix

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
+        // Way\Generators\GeneratorsServiceProvider::class,
         Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
 
         /*
